c5c1b2b30ecea71b4acdd1bdfb557f14b7ae5179
Boulay <cbmagala@gmail.com>
2024-10-09 16:51:40 +0200

    test CRUD user

--- test/UserTest.js
+++ test/UserTest.js
@@ -6,6 +6,7 @@ import { createUser,getUser,deleteUser,Connected,updateUser } from "../controlle
 import User from "../models/UserModel.js";
 
 let headers = {};
+let id=null;
 
 const request = supertest(app);
 
@@ -22,7 +23,7 @@ describe("User routes", () => {
                 password: "testpassword"
             });
             expect(response.status).to.equal(201);
-            expect(response.body).to.have.property("message", "User created successfully");
+            expect(response.body.token).to.exists();
         });
 
         it("Connexion avec un utilisateur", async () => {
@@ -42,12 +43,12 @@ describe("User routes", () => {
     
             // Register a user and store the token in the headers
             const response = await request.post("RailRoad/users/register").send({
-                email: "admin",
+                email: "admin@example.com",
                 password: "admin",
                 username: "admin",
             });
             const token = response.body.token;
-    
+            id = response.body.id;
             headers = {
                 Authorization: `Bearer ${token}`,
             };
@@ -59,7 +60,27 @@ describe("User routes", () => {
         })
 
         it("Récupération d'un utilisateur", async () => {
-           const response = await request.get("/RailRoad/users/5f7661007985850011469624").set(
+           const response = await request.get("/RailRoad/users/"+id).set(headers);
+           expect(response.status).to.equal(200);
+           expect(response.body).to.be.an("object");
+           expect(response.body.username).to.be.equal("admin");
+           expect(response.body.email).to.be.equal("admin@example.com");
+
+        });
+
+        it("Mise à jour d'un utilisateur", async () => {
+            const response = await request.put("/RailRoad/users/"+id).set(headers).send({
+                username: "newadmin",
+                email: "newadmin@example.com",
+                password: "newpassword",
+            });
+            expect(response.status).to.equal(201);
+            expect(response.body).to.be.equal("User updated successfully");
+        });
+        it("Suppression d'un utilisateur", async () => {
+            const response = await request.delete("/RailRoad/users/"+id).set(headers);
+            expect(response.status).to.equal(200);
+            expect(response.body).to.be.equal("User deleted successfully");
         });
     });
 });
