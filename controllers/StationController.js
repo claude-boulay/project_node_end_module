66d3954ec14c41a15f22e0529cd65bc4311df80c
Boulay <cbmagala@gmail.com>
2024-10-13 11:15:49 +0200

    Sation controllers and router

---
+++ controllers/StationController.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import mongoose from "mongoose";
+import StationsModel from "../models/StationModel";
+
+//création en base de donnée de la station de train avec les données fournies
+//imageLink doit correspondre au chemin de l'image enregistrer sur le server lors de la création
+export function createStation(name, city, open_hour, close_hour, address, imageLink) {
+    const station = {name, city, open_hour, close_hour, address, image: imageLink};
+    return StationsModel.create(station);
+}
+
+export async function getAllStations() {
+    return await StationsModel.find();
+}
+
+export async function getStationById(id) {
+    return await StationsModel.findById(id);
+}
+
+export async function updateStation(id, content) {
+    await StationsModel.findByIdAndUpdate(id, { $set: content });
+}
+
+export async function deleteStation(id) {
+    await StationsModel.findByIdAndDelete(id);
+    //ajouter la suppression de tout les trains relier à cette stations su start station or end stations
+}
